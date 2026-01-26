dc606ae611b270bc54a9a18789f695d2b52dac39
PatrikS24 <patriksimonsen3@gmail.com>
2026-01-26 04:43:00 +0200

    Update chart visuals

--- app/src/main/java/com/example/trainingtracker/ui/statistics/GeneralStatisticsScreen.kt
+++ app/src/main/java/com/example/trainingtracker/ui/statistics/GeneralStatisticsScreen.kt
@@ -1,8 +1,16 @@
 package com.example.trainingtracker.ui.statistics
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.trainingtracker.controller.statistics.GeneralStatisticsViewModel
@@ -15,9 +23,12 @@ import com.patrykandpatrick.vico.compose.cartesian.rememberVicoScrollState
 import com.patrykandpatrick.vico.compose.cartesian.rememberVicoZoomState
 import com.patrykandpatrick.vico.compose.common.component.rememberLineComponent
 import com.patrykandpatrick.vico.compose.common.component.rememberTextComponent
+import com.patrykandpatrick.vico.compose.common.shader.toShaderProvider
+import com.patrykandpatrick.vico.compose.common.shape.toVicoShape
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
+import com.patrykandpatrick.vico.core.cartesian.layer.ColumnCartesianLayer
 import com.patrykandpatrick.vico.core.common.Fill
 import java.time.temporal.WeekFields
 import java.util.Locale
@@ -26,7 +37,14 @@ import kotlin.math.roundToInt
 
 @Composable
 fun GeneralStatisticsScreen(viewModel: GeneralStatisticsViewModel = viewModel()) {
-    WorkoutFrequencyChart(viewModel)
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(18.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Workouts per week")
+        WorkoutFrequencyChart(viewModel)
+    }
 }
 
 @Composable
@@ -55,13 +73,31 @@ fun WorkoutFrequencyChart(viewModel: GeneralStatisticsViewModel) {
 
     CartesianChartHost(
         chart = rememberCartesianChart(
-            rememberColumnCartesianLayer(),
+            rememberColumnCartesianLayer(
+                columnProvider = ColumnCartesianLayer.ColumnProvider.series(
+                    rememberLineComponent(
+                        fill = Fill(
+                            shaderProvider = Brush.verticalGradient(
+                                listOf(
+                                    MaterialTheme.colorScheme.primary,
+                                    MaterialTheme.colorScheme.primary.copy(alpha = 0.4f)
+                                )
+                            ).toShaderProvider()
+                        ),
+                        thickness = 16.dp,
+                        shape = MaterialTheme.shapes.small.toVicoShape()
+                    )
+                )
+            ),
             bottomAxis = HorizontalAxis.rememberBottom(
                 valueFormatter = labelFormatter,
                 label = rememberTextComponent(color = MaterialTheme.colorScheme.onSurface, textSize = 12.sp),
                 guideline = null
                 ),
-            startAxis = VerticalAxis.rememberStart(),
+            startAxis = VerticalAxis.rememberStart(
+                itemPlacer = VerticalAxis.ItemPlacer.step({ 1.0 }),
+                valueFormatter = CartesianValueFormatter.decimal(java.text.DecimalFormat("#"))
+            ),
         ),
         modelProducer = viewModel.modelProducer,
         scrollState = scrollState,
